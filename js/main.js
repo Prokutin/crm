7c115ea6c00647ff88990fef7da6edc0c9a2d32f
Prokutin <q152827060@yandex.ru>
2023-03-08 03:53:17 +0300

    module2-task2

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@
 const modalTitle = document.querySelector('.modal__title');
 const modalForm = document.querySelector('.modal__form');
 const modalCheckbox = document.querySelector('.modal__checkbox');
-const modalDiscount = document.querySelector('.input_discount');
+const modalDiscount = document.querySelector('.modal__input');
+console.log('modalDiscount: ', modalDiscount);
